87c1ff5c9938c1a72b111b98c7ee426ea00f2a76
SteshM <mbithestella0@gmail.com>
2024-10-17 11:17:26 +0300

    Added redis configuration

---
+++ KYC/src/main/java/com/example/KYC/Configurations/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.example.KYC.Configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig = new RedisStandaloneConfiguration();
+        redisStandaloneConfig.setHostName(redisHost);
+        redisStandaloneConfig.setPort(redisPort);
+        return new LettuceConnectionFactory(redisStandaloneConfig);
+    }
+
+}
